60aaacf2ab209fc97b383ae15a0016afc0df3bd1
yungmeech <amechia99@gmail.com>
2025-06-01 15:31:01 -0400

    edited colorscheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#F84E56",
-        secondary: "#DE2A32",
-        tertiary: "#C5080F",
-        quaternary: "#AB060C",
+        primary: "#06509B",
+        secondary: "#0760B2",
+        tertiary: "#1F7CC9",
+        quaternary: "#45A1E4",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
-        customtext: "#172534",
+        customtext: "#FFFFFF",
       },
     },
   },
